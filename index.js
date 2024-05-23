dfa5777cc1b4f491afdab755e4f60f8e186b4294
sayonil-mitra <139944160+sayonil-mitra@users.noreply.github.com>
2024-05-23 11:03:03 +0530

    test

--- index.js
+++ index.js
@@ -9,26 +9,32 @@ app.use(cors());
 // Middleware to serve static files from the current directory
 app.use(express.static(__dirname));
 
-app.get("/opened/:emailMessageId", (req, res) => {
-  let uniqueId = decodeURI(req.params?.emailMessageId);
-  console.log("image fetched on message: ", uniqueId);
+// check email with specific id has been opened by specific user
+app.get("/opened/:email/:uniqueId", (req, res) => {
+  let emailId = decodeURI(req.params?.email);
+  let uniqueId = decodeURI(req.params?.uniqueId);
+  console.log(`Email opened. Email: ${emailId}, unique id: ${uniqueId}`);
 
   res.sendFile(path.join(__dirname, "tracker.jpg"));
 
-  if (emailOpenRecords.has(uniqueId)) {
-    emailOpenRecords.set(uniqueId, true);
+  let emailIdentifer = `${emailId}-${uniqueId}`;
+  if (emailOpenRecords.has(emailIdentifer)) {
+    emailOpenRecords.set(emailIdentifer, true);
   }
 });
 
+// serve static image
 app.get("/opened", (req, res) => {
   res.sendFile(path.join(__dirname, "tracker.jpg"));
 });
 
+// check status of all emails sent
 app.get("/check/open", (req, res) => {
   res.json(Object.fromEntries(emailOpenRecords));
   res.end();
 });
 
+// record which email has been sent
 app.get("/record_send/:email/:uniqueId", (req, res) => {
   let uniqueId = req.params.uniqueId;
   let email = decodeURI(req.params.email);
